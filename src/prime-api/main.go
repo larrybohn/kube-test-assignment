8769e4eb447984ddae9247cfb37e86d460a52ce4
larrybohn <larrybohn@users.noreply.github.com>
2025-01-27 01:20:11 +0100

    Added startup delay to the app, added basic logic to wait for pod readiness.

--- src/prime-api/main.go
+++ src/prime-api/main.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 // Main function to set up routes and start the server
 func main() {
 	http.HandleFunc("/is-prime/", isPrimeHandler)
 	http.HandleFunc("/next-prime/", nextPrimeHandler)
-	http.HandleFunc("/health-check", healthCheckHandler)
+	http.HandleFunc("/healthz", healthCheckHandler)
 
 	port := ":8080"
+	fmt.Println("Starting to sleep 30 seconds")
+	time.Sleep(30 * time.Second)
 	fmt.Printf("Starting server on port %s...\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
